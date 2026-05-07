66eb984c5698930211d4bc8d4e7d22b6d0139e2c
gustavolakre <gustavo.lakre@gmail.com>
2026-05-07 16:54:54 -0300

    Conecta PostgreSQL

--- server.js
+++ server.js
@@ -3,13 +3,37 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 
+const pool = require("./src/db/database")
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("API ONLINE")
+app.get("/", async (req, res) => {
+
+  try {
+
+    const result = await pool.query("SELECT NOW()")
+
+    res.json({
+      online: true,
+      banco: true,
+      horario: result.rows[0]
+    })
+
+  } catch (err) {
+
+    console.error(err)
+
+    res.status(500).json({
+      online: true,
+      banco: false,
+      erro: err.message
+    })
+
+  }
+
 })
 
 const PORT = process.env.PORT || 3000
