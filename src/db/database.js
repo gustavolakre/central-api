2e6e909f0914262c9fe01ebae7a492d884443984
gustavolakre <gustavo.lakre@gmail.com>
2026-05-18 09:24:30 -0300

    corrige integração pipefy

--- src/db/database.js
+++ src/db/database.js
@@ -4,15 +4,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  },
-
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000
-})
-
-pool.on("error", (err) => {
-  console.error("ERRO POSTGRES:", err)
+  }
 })
 
 module.exports = pool
