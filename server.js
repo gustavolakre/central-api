eddd7bd90aa4c5d2eee50b3c6e3c7468d934fae2
gustavolakre <gustavo.lakre@gmail.com>
2026-05-18 11:55:19 -0300

    debug imports

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 require("dotenv").config()
 
 const express = require("express")
+const cors = require("cors")
+
+console.log("1 - express ok")
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+console.log("2 - middlewares ok")
+
+// TESTE IMPORTS
+require("./src/db/database")
+console.log("3 - database ok")
+
+require("./routes/importarPipefy")
+console.log("4 - importarPipefy ok")
+
+require("./routes/importarParceiros")
+console.log("5 - importarParceiros ok")
+
+require("./routes/gerarFaturamento")
+console.log("6 - gerarFaturamento ok")
+
+require("./routes/buscarCargas")
+console.log("7 - buscarCargas ok")
+
+require("./routes/buscarParceiros")
+console.log("8 - buscarParceiros ok")
 
 app.get("/", (req, res) => {
   res.send("API ONLINE")
