be974ce0bde15146daefb9c634d93b709b77e6f7
gustavolakre <gustavo.lakre@gmail.com>
2026-05-18 11:05:08 -0300

    corrige integração pipefy

--- server.js
+++ server.js
@@ -61,6 +61,6 @@ app.get("/", async (req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor rodando na porta", PORT)
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
