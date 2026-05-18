020890b4f3af3c0c7762b9d69bf0d52741c8e6ad
gustavolakre <gustavo.lakre@gmail.com>
2026-05-18 11:52:19 -0300

    teste minimo railway

--- server.js
+++ server.js
@@ -1,68 +1,11 @@
 require("dotenv").config()
 
-const path = require("path")
 const express = require("express")
-const cors = require("cors")
-
-const importarPipefy = require("./routes/importarPipefy")
-const importarParceiros = require("./routes/importarParceiros")
-const gerarFaturamento = require("./routes/gerarFaturamento")
-const buscarCargas = require("./routes/buscarCargas")
-const buscarParceiros = require("./routes/buscarParceiros")
-
-const pool = require("./src/db/database")
 
 const app = express()
 
-app.use(cors())
-app.use(express.json())
-
-// 🔥 COMENTE TEMPORARIAMENTE ESTES STATIC
-// app.use(
-//   express.static(
-//     path.join(__dirname, "faturamento")
-//   )
-// )
-
-// app.use(
-//   express.static(
-//     path.join(__dirname, "frontend")
-//   )
-// )
-
-// ROTAS
-app.use("/importar-pipefy", importarPipefy)
-app.use("/importar-parceiros", importarParceiros)
-app.use("/gerar-faturamento", gerarFaturamento)
-
-app.use("/buscarDados", buscarCargas)
-app.use("/buscarParceiros", buscarParceiros)
-
-// ROTA TESTE
-app.get("/", async (req, res) => {
-
-  try {
-
-    const result = await pool.query("SELECT NOW()")
-
-    res.json({
-      online: true,
-      banco: true,
-      horario: result.rows[0]
-    })
-
-  } catch (err) {
-
-    console.error(err)
-
-    res.status(500).json({
-      online: true,
-      banco: false,
-      erro: err.message
-    })
-
-  }
-
+app.get("/", (req, res) => {
+  res.send("API ONLINE")
 })
 
 const PORT = process.env.PORT || 3000
