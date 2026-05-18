1715d1944580b6fe40d51b3fb852b37eb8fe8f49
gustavolakre <gustavo.lakre@gmail.com>
2026-05-18 17:28:27 -0300

    ajuste no script 2

--- routes/buscarCargas.js
+++ routes/buscarCargas.js
@@ -8,10 +8,19 @@ router.get("/", async (req, res) => {
   try {
 
     const result = await pool.query(`
-      SELECT *
-      FROM controle_cargas
-    
-    `)
+  SELECT
+    pipefy_card_id         AS "Código",
+    comprador              AS "Comprador",
+    fornecedor             AS "Fornecedor",
+    quantidade             AS "Quantidade",
+    tipo_suino             AS "Tipo Suíno",
+    mortos_transporte      AS "Mortos em transporte",
+    nf_taxa_compr          AS "NF Taxa Compr.",
+    nf_taxa_fornec         AS "NF Taxa Fornec.",
+    etiquetas              AS "Etiquetas",
+    nota_fiscal_venda      AS "N Nota de Venda"
+  FROM controle_cargas
+`)
 
     res.json(result.rows)
 
