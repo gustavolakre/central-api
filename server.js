12143d718962c57ca2f510bc3cf08c49fe385340
gustavolakre <gustavo.lakre@gmail.com>
2026-05-18 11:29:08 -0300

    corrige server

--- server.js
+++ server.js
@@ -61,6 +61,10 @@ app.get("/", async (req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+app.get("/", (req, res) => {
+  res.send("API ONLINE")
+})
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`)
 })
