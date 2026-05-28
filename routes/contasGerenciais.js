9816a3b5874fc8d5b64787073cb283bf5d57009b
gustavolakre <gustavo.lakre@gmail.com>
2026-05-28 14:08:13 -0300

    ajuste nas contas gerenciais

--- routes/contasGerenciais.js
+++ routes/contasGerenciais.js
@@ -4,10 +4,6 @@ const router = express.Router()
 const pool = require("../src/db/database")
 
 
-/* =========================================
-   LISTAR
-========================================= */
-
 router.get("/", async(req,res)=>{
 
     try{
@@ -36,59 +32,5 @@ router.get("/", async(req,res)=>{
 
 })
 
-
-
-/* =========================================
-   NOVA CONTA
-========================================= */
-
-router.post("/", async(req,res)=>{
-
-    try{
-
-        const {
-            nome,
-            tipo
-        } = req.body
-
-        const result = await pool.query(`
-
-            INSERT INTO contas_gerenciais (
-
-                nome,
-                tipo
-
-            )
-
-            VALUES (
-
-                $1,$2
-
-            )
-
-            RETURNING *
-
-        `,[
-
-            nome,
-            tipo
-
-        ])
-
-        res.json(result.rows[0])
-
-    }
-
-    catch(err){
-
-        console.error(err)
-
-        res.status(500).json({
-            erro: err.message
-        })
-
-    }
-
-})
 
 module.exports = router
