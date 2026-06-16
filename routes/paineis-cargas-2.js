c4419a4864eb3f1df338ca61f908202b605c456d
gustavolakre <gustavo.lakre@gmail.com>
2026-06-16 11:09:08 -0300

    ajuste na topbar do dash 28

--- routes/paineis-cargas-2.js
+++ routes/paineis-cargas-2.js
@@ -1,54 +1,53 @@
 const express = require("express");
 const router = express.Router();
-
 const pool = require("../src/db/database");
 
-router.get("/", async (req, res) => {
+router.get("/", async (req,res)=>{
 
-    try {
+    try{
 
         const resultado = await pool.query(`
 
             SELECT
 
                 substring(
-                    c.etiquetas
+                    cc.etiquetas
                     from '[0-9]{4}/[0-9]{2}'
                 ) as semana,
 
-                c.fornecedor,
+                cc.fornecedor,
 
-                c.comprador,
+                cc.comprador,
 
-                COALESCE(
-                    pf.uf,
-                    'N/A'
-                ) as estado_fornecedor,
+                cc.tipo_suino,
 
-                COALESCE(
-                    pc.uf,
-                    'N/A'
-                ) as estado_comprador,
+                pf.uf as estado_fornecedor,
+
+                pc.uf as estado_comprador,
 
                 SUM(
-                    COALESCE(c.quantidade,0)
-                ) as total_suinos
+                    COALESCE(cc.quantidade,0)
+                ) as quantidade
 
-            FROM controle_cargas c
+            FROM controle_cargas cc
 
             LEFT JOIN parceiros_negocio pf
-                ON trim(lower(pf.nome_usual))
-                 = trim(lower(c.fornecedor))
+                ON pf.nome_usual = cc.fornecedor
 
             LEFT JOIN parceiros_negocio pc
-                ON trim(lower(pc.nome_usual))
-                 = trim(lower(c.comprador))
+                ON pc.nome_usual = cc.comprador
+
+            WHERE substring(
+                cc.etiquetas
+                from '[0-9]{4}/[0-9]{2}'
+            ) IS NOT NULL
 
             GROUP BY
 
                 semana,
-                c.fornecedor,
-                c.comprador,
+                cc.fornecedor,
+                cc.comprador,
+                cc.tipo_suino,
                 pf.uf,
                 pc.uf
 
@@ -58,7 +57,7 @@ router.get("/", async (req, res) => {
 
         res.json(resultado.rows);
 
-    } catch (erro) {
+    }catch(erro){
 
         console.error(erro);
 
