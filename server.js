afe8818a601d391f71668d1aa428e97d28795b62
gustavolakre <gustavo.lakre@gmail.com>
2026-06-16 09:55:12 -0300

    ajuste na topbar do dash 13

--- server.js
+++ server.js
@@ -40,6 +40,9 @@ const dashboard =
 const paineisCargas =
     require("./routes/paineis-cargas");
 
+const paineisCargas =
+    require("./routes/paineis-cargas-2");
+
 app.use(cors())
 app.use(express.json())
 
@@ -124,6 +127,11 @@ app.use(
 
 app.use(
     "/paineis-cargas",
+    paineisCargas
+);
+
+app.use(
+    "/paineis-cargas-2",
     paineisCargas
 );
 
