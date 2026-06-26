4a1370ae785f4f3d057c4c028d06b4677ed60939
gustavolakre <gustavo.lakre@gmail.com>
2026-06-26 13:45:01 -0300

    ajuste na mensagem de erro no faturamento 3

--- server.js
+++ server.js
@@ -90,7 +90,8 @@ io.on("connection", (socket) => {
 });
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 
 // Login público
 app.get("/login", (req, res) => {
